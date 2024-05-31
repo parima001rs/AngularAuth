ed56e90ff65ce4cd6c173c73cf8cbcdde6861a6b
Parima <parima@alletec.com>
2024-05-31 18:18:54 +0530

    reset-password-success

--- src/app/components/reset/reset.component.ts
+++ src/app/components/reset/reset.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ConfirmPasswordValidator } from 'src/app/helpers/confirm-password.validator';
+import ValidateForm from 'src/app/helpers/validateform';
+import { ResetPassword } from 'src/app/models/reset-password.model';
+import { ResetPasswordService } from 'src/app/services/reset-password.service';
 
 @Component({
   selector: 'app-reset',
@@ -6,10 +12,70 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./reset.component.scss']
 })
 export class ResetComponent implements OnInit {
+  resetPasswordForm!: FormGroup;
+    emailToReset!: string;
+    emailToken!: string;
+    resetPasswordObj = new ResetPassword();
 
-  constructor() { }
+  constructor(
+    private fb: FormBuilder,
+    private activatedRoute: ActivatedRoute,
+    private resetService: ResetPasswordService,
+    private router: Router,
+    // private toast: NgToastService
+  ) { }
 
   ngOnInit(): void {
+    this.resetPasswordForm = this.fb.group({
+      password: [null, Validators.required],
+      confirmPassword: [null, Validators.required]
+    },{
+      validator: ConfirmPasswordValidator("password", "confirmPassword")
+    });
+
+    this.activatedRoute.queryParams.subscribe(val =>{
+      this.emailToReset = val['email'];
+      let uriToken = val['code'];
+      this.emailToken = uriToken.replace(/ /g,'+');
+      console.log(this.emailToken);
+      console.log(this.emailToReset);
+    })
   }
 
+  
+
+  reset(){
+    if(this.resetPasswordForm.valid){
+      console.log(this.resetPasswordForm.value);
+
+      this.resetPasswordObj.email = this.emailToReset;
+      this.resetPasswordObj.newPassword = this.resetPasswordForm.value.password;
+      this.resetPasswordObj.confirmPassword = this.resetPasswordForm.value.confirmPassword;
+      this.resetPasswordObj.emailToken = this.emailToken;
+
+      this.resetService.resetPassword(this.resetPasswordObj)
+      .subscribe({
+        next:(res) =>{
+          // this.toast.success({detail: 'SUCCESS',summary: "Password Reset Successfully!", duration:3000});
+          alert("Password reset successfully");
+          this.router.navigate(['/'])
+        },
+        error:(err)=>{
+          // this.toast.error({detail: 'ERROR',summary: "Something went wrong", duration:3000});
+          alert("something went wrong");
+        }
+      })
+    }
+    else{
+      ValidateForm.validateAllFormFields(this.resetPasswordForm);
+      console.log("Your form is invalid");
+      //logic for throwing error
+    }
+  }
+
+  
+
+  
+
 }
+
