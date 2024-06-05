70500fb305ece314b27f3a9c6d7f98fc5efa6480
Parima <parima@alletec.com>
2024-06-05 23:47:24 +0530

    table is working perfectly

--- src/app/components/table/table.component.ts
+++ src/app/components/table/table.component.ts
@@ -2,20 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { from, map, mergeMap, switchMap, toArray, Observable } from 'rxjs';
 import { ApiService } from 'src/app/services/api.service';
 
-// interface Customer {
-//   customerId: string;
-//   name: string;
-//   email: string;
-//   devices?: Device[]; // Optional property to hold devices
-// }
-
-// interface Device {
-//   deviceId: string;
-//   applicationId: string;
-//   startDate: string;
-//   endDate: string;
-//   IsPlanActive: boolean;
-// }
 
 @Component({
   selector: 'app-table',
@@ -25,39 +11,27 @@ import { ApiService } from 'src/app/services/api.service';
 export class TableComponent implements OnInit {
   public customers: any = [];
   // public customers: Customer[] = [];
-  // public devices: any = [];
+  public devices: any = [];
+  
+  public isPlanActive(value: number): boolean {
+    return value === 1;
+  }
 
   constructor(private api: ApiService) { }
 
   ngOnInit(): void {
+
     this.api.getCustomers()
-    .subscribe(res=>{
-      this.customers = res;
-    });
-
-      // this.api.getCustomers()
-      //   .subscribe(customers => {
-      //     this.customers = customers;
-      //     this.customers.forEach(customer => {
-      //       this.api.getDevicesById(customer.customerId)
-      //         .subscribe(devices => {
-      //           customer.devices = devices; // Add a new property to each customer for their devices
-      //         });
-      //     });
-      //   });
-
-    // this.api.getCustomers()
-    // .subscribe(res => {
-    //   this.customers = res;
-    //   res.forEach(customer => {
-    //     this.api.getDevicesById(customer.customerId)
-    //       .subscribe(devices => {
-    //         customer.devices = devices; // Add a new property to each customer for their devices
-    //       });
-    //   });
-    // });
-
-  
+      .subscribe(customers => {
+        this.customers = customers;
+        this.customers.forEach((customer: { customerId: string; devices: any[]; }) => {
+          this.api.getDevicesById(customer.customerId)
+            .subscribe(devices => {
+              customer.devices = devices; // Add a new property to each customer for their devices
+              console.log(devices);
+            });
+        });
+      });
 
   }
 
